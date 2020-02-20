fb4fa1b421679774d2f565a585407703f4abe5c4
haizai <haizainaive@gmail.com>
2020-02-20 14:12:37 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RetroSnaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
